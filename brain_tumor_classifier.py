4c3b53d3c011a1a07804869d47fce2926f0ba5d2
AmirRazavi121303 <amirsh.razavi@gmail.com>
2025-07-09 23:39:45 +0200

    working version (only ran on kaggle notebook)

--- brain_tumor_classifier.py
+++ brain_tumor_classifier.py
@@ -1,121 +1,104 @@
+#general
 import torch
 import torch.nn as nn
+from torchvision import transforms
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms
 from torchvision.datasets import ImageFolder
-import torchvision.models 
-from torchvision.models  import ResNet50_Weights
-import timm
-import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-from PIL import Image
 
-#cleans the data pretty much, makes it easy for the program to read
-class BrainMRIDataset(Dataset):
-    def __init__(self, data_dir, transform=None): 
-        self.data = ImageFolder(data_dir, transform=transform)
-        
+#for prebuilt models
+import torchvision.models as models
+from torchvision.models import ResNet50_Weights
+import timm
+
+#for NN from scratch
+import torch.nn.functional as F
+
+#visualize
+from PIL import Image
+import matplotlib.pyplot as plt
+
+#-----classifying the data-----
+class BrainTumorClassifier(Dataset):
+    
+    #initialize data by importing the image from data_dir and setting transforms
+    def __init__(self, data_dir, transform = None):
+        self.data = ImageFolder(data_dir, transform = transform) 
+
+    #returns total number of samples in dataset
     def __len__(self):
         return len(self.data)
+
+    #retrieves a single sample with image and label
     def __getitem__(self, idx):
         return self.data[idx]
+
+    #provides a property (flexible mechanism) that returns class names from dataset
     @property
     def classes(self):
         return self.data.classes
 
-#according to the dataset owner a lot of the data might be in wrong sizes, so lets do this:
 transform = transforms.Compose([
-    transforms.Resize((224, 224)), 
-    transforms.ToTensor(),
-    transforms.Normalize([0.5, 0.5, 0.5], [0.5,0.5,0.5])
+    transforms.Resize((224, 224)), #resizes each image to 224 x 224 pixels
+    transforms.ToTensor(), #converts the entire image or array into a tensor, scales pixel values 0 - 1
+    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]), #normalizes each rgb to have a mean & stdev of 0.5
 ])
 
-data_dir = "/Users/amir/Downloads/CodeAmir/BrainTumorClassifier/brain mri scans/Training"
-dataset = BrainMRIDataset(data_dir, transform)
+data_dir = "/kaggle/input/brain-mri-scans-2/Training"
+dataset = BrainTumorClassifier(data_dir, transforms)
 
-#building the model itself
-class TumorClassifier(nn.Module):
-    def __init__(self, num_classes=4): #initialize all the parameters of the model
-        super(TumorClassifier, self).__init__()
-        self.base_model = timm.create_model('efficientnet_b0', pretrained=True)
-        #this next line simply cuts off the last layer of the timm model and implaments our 4 output version
-        self.features = nn.Sequential(*list(self.base_model.children())[:-1])
-        model_out_size = 1280 
-        #lets make a classifier to narrow down the outputs of the model from 1280 to 4
-        self.classifier = nn.Linear(model_out_size, num_classes)
-    
-    def forward(self, x): #connect the parts and give an output
-        x = self.features(x)
-        output = self.classifier(x)
-        return output
-
-#print(example_out.shape)
-#pill print something like 3, 32, 224, 224,
-
-#-----creating the training loop-----
-model = TumorClassifier(num_classes = 4)
-#model = torchvision.models.resnet50(weights= ResNet50_Weights."IMAGENET1K_V1")
-
-#loss function
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.0001)
-
-#here everything became much more clear to me
 #this loads in the data
 train_folder = "/Users/amir/Downloads/CodeAmir/BrainTumorClassifier/brain mri scans/Training"
 test_folder = "/Users/amir/Downloads/CodeAmir/BrainTumorClassifier/brain mri scans/Testing"
 
 #this makes the data readable for the program
-train_data = BrainMRIDataset(train_folder, transform = transform)
-test_data = BrainMRIDataset(test_folder, transform = transform)
+train_data = BrainTumorClassifier(train_folder, transform = transform)
+test_data = BrainTumorClassifier(test_folder, transform = transform)
 
 #this feeds the data optimally to the program. 
-#it takes the data, turns them into 32 pixel chunks, which is more aligned with CNN's and makes the 
-#results of the model more optimal. The shuffle part makes sense for training data as we want it to be random
-train_loader = DataLoader(train_data, batch_size = 32, shuffle=True)
-test_loader = DataLoader(test_data, batch_size = 32, shuffle=False)
+training_loader = DataLoader(train_data, batch_size = 32, n_workers= 2, shuffle=True)
+testing_loader = DataLoader(test_data, batch_size = 32, n_workers = 2, shuffle=False)
 
-device = torch.device("mps" if torch.mps.is_available() else "cpu") 
+#-----check to see if the finished data shape and labels are correct for the model-----
+for image, label in training_loader:
+    break
+print(image.shape) 
+print(label[0:10])
+print(torch.unique(label))
 
-num_epoch = 5
-train_losses, val_losses = [], []
+#-----initializing model-----
+model = models.resnet50(weights=ResNet50_Weights.IMAGENET1K_V2) #loads in the model
+model.fc = nn.Linear(model.fc.in_features, 4) #connects the final layer of the model to a layer with 4 outputs
+loss_function = nn.CrossEntropyLoss() #calculates our loss function
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001) #gradient descent optimizer
 
+#lets tell the program we want to do this with our gpu (in this case cuda)
+device = torch.device("mps" if torch.mps.is_available() else "cpu")
+
+#training loop
+num_epochs = 20
+train_losses = []
 model.to(device)
 
-#this next part is the training loop
-
-for e in range(num_epoch):
-    #set the model to train
-    model.train()
-    running_loss = 0.0
-    for images, labels in train_loader:
-        images, labels = images.to(device), labels.to(device)
+for epoch in range(num_epochs):
+    running_loss = 0.0 #initialize a running loss
+    model.train() 
+    for image, label in training_loader:
+        image, label = image.to(device), label.to(device)
         optimizer.zero_grad()
-        outputs = model(images)
-        loss = criterion(outputs, labels)
+        outputs = model(image)
+        loss = loss_function(outputs, label)
         loss.backward()
         optimizer.step()
-        running_loss += loss.item() * images.size(0)
-        
-    train_loss = running_loss / len(train_loader.dataset)
+        running_loss += loss.item() * image.size(0)
+    train_loss = running_loss / len(training_loader.dataset)
     train_losses.append(train_loss)
+    print(running_loss)
+    print(f"Epoch {epoch+1}/{num_epochs} - Train Loss: {train_loss:.4f}")
 
-    model.eval()
-    running_loss = 0.0
-    with torch.no_grad():
-        for images, labels in test_loader:
-            images, labels = images.to(device), labels.to(device)
-            outputs = model(images)
-            loss = criterion(outputs, labels)
-            running_loss += loss.item() * images.size(0)
-
-        val_loss = running_loss / len(test_loader.dataset)
-        val_losses.append(val_loss)
-
-    print(f"Epoch {e + 1}/{num_epoch} - Train Loss: {train_loss}, Validation Loss: {val_loss}")
-
+#-----visualize-----
 #load and preprocess image
 def image_process(image_path, transform):
     image = Image.open(image_path).convert("RGB")
@@ -128,7 +111,7 @@ def predict(model, image_tensor, device):
         image_tensor = image_tensor.to(device)
         outputs = model(image_tensor)
         probabilities = torch.nn.functional.softmax(outputs, dim=1)
-    return probabilities.cpu().np().flatten()
+    return probabilities.cpu().numpy().flatten()
 
 #visualization
 def visualize(original_image, probabilities, class_names):
@@ -148,8 +131,7 @@ def visualize(original_image, probabilities, class_names):
     plt.show()
 
 #example
-test_image = "/Users/amir/Downloads/CodeAmir/BrainTumorClassifier/brain mri scans/Testing/glioma/Te-gl_0010.jpg"
-
+test_image = "path to test image"
 original_image, image_tensor = image_process(test_image, transform)
 probabilities = predict(model, image_tensor, device)
 
